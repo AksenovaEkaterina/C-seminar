abd150c0b3b9701022f371befc116f3b08488528
Ekaterina <Kuteynikova.ekaterina@mail.ru>
2022-04-27 20:31:41 +0300

    Add s_1

---
+++ S_1/Program.cs
@@ -0,0 +1,12 @@
+﻿string s_a = Console.ReadLine();
+string s_b = Console.ReadLine();
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+if (a>b){
+    Console.Write("max = ");
+    Console.Write(a);
+}
+else {
+    Console.Write("max = ");
+    Console.Write(b); 
+}
