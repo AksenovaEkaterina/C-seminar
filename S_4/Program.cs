06692df8696b3e7964a819a7047aa4c2b7296fb3
Ekaterina <Kuteynikova.ekaterina@mail.ru>
2022-04-27 21:18:57 +0300

    Add s_4

---
+++ S_4/Program.cs
@@ -0,0 +1,15 @@
+﻿string s_x = Console.ReadLine();
+int x = int.Parse(s_x);
+int y = 1;
+while (y <= x)
+{
+    if (y % 2 == 0)
+    {
+        Console.Write($"{y},");
+        y++;
+    }
+    else
+    {
+        y++;
+    }
+}
